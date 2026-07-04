2c0bcc9346b62a990040228f52faf384d05e9287
Shankara Mani Sai Nookalamanthi <143930707+mani-sai-12@users.noreply.github.com>
2026-07-05 00:30:33 +0530

    Time: 0 ms (100%), Space: 19.3 MB (44.79%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.py
+++ 2469-convert-the-temperature/2469-convert-the-temperature.py
@@ -1,4 +1,5 @@
 class Solution:
     def convertTemperature(self, celsius: float) -> List[float]:
-        ans=[(celsius+273.15),(celsius*(1.80)+32.00)]
-        return ans
+        k=celsius + 273.15
+        f=celsius * 1.80 + 32.00
+        return [k,f]
