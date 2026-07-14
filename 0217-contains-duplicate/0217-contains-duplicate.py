2b456d592bccb5bf5e62afe357c6a5a50c4fd7cb
Shankara Mani Sai Nookalamanthi <143930707+mani-sai-12@users.noreply.github.com>
2026-07-14 07:04:07 +0530

    Time: 63 ms (5.17%), Space: 29.9 MB (98.25%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,13 +1,20 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        d={}
-        for num in nums:
-            if num not in d:
-                d[num]=1
-            else:
-                d[num]+=1
-        for key,value in d.items():
-            if value>=2:
+        n=len(nums)
+        nums.sort()
+        for i in range(n-1):
+            j=i+1
+            if nums[i]==nums[j]:
                 return True
         return False
+        # d={}
+        # for num in nums:
+        #     if num not in d:
+        #         d[num]=1
+        #     else:
+        #         d[num]+=1
+        # for key,value in d.items():
+        #     if value>=2:
+        #         return True
+        # return False
         
