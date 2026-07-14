69fead26d220981cf821da90cf3245f5d8f7bd35
Shankara Mani Sai Nookalamanthi <143930707+mani-sai-12@users.noreply.github.com>
2026-07-14 23:30:19 +0530

    Time: 6 ms (93.21%), Space: 19.5 MB (45.41%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        a=Counter(s)
+        b=Counter(t)
+        if a==b:
+            return True
+        else:
+            return False
+        
