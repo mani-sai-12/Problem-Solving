4d4cfcdf4ebd9cc313745fc16d6e10d125ed181f
Shankara Mani Sai Nookalamanthi <143930707+mani-sai-12@users.noreply.github.com>
2026-07-14 07:19:41 +0530

    Time: 56 ms (5.17%), Space: 30 MB (96.05%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,9 +1,14 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        a=Counter(nums)
-        first=a.most_common(1)[0]
-        if first[1]>=2:
-            return True
-        else:
-            return False
+        nums.sort()
+        for i in range(1,len(nums)):
+            if nums[i]==nums[i-1]:
+                return True
+        return False
+        # a=Counter(nums)
+        # first=a.most_common(1)[0]
+        # if first[1]>=2:
+        #     return True
+        # else:
+        #     return False
         
